b5c2f187b14b9ee40caef524ff0e61b7039c7f32
Daniel Findell <Daniel.Findell@chrobinson.com>
2022-01-21 21:26:43 -0600

    updated

--- src/avi.rs
+++ src/avi.rs
@@ -13,10 +13,13 @@ type BoxResult<T> = Result<T,Box<dyn Error + Send + Sync>>;
 #[derive(Debug, Clone)]
 pub struct AviClient {
     client: reqwest::Client,
-    expires: i64,
+    token_expires: i64,
+    session_expires: i64,
     username: String,
     password: String,
     controller: String,
+    tenant: String,
+    version: String,
     token: String,
     session: String
 }
@@ -35,12 +38,15 @@ impl AviClient {
         let username = opts.value_of("username").unwrap().to_string();
         let password = opts.value_of("password").unwrap().to_string();
         let controller = opts.value_of("controller").unwrap().to_string();
+        let tenant = opts.value_of("tenant").unwrap().to_string();
+        let version = opts.value_of("version").unwrap().to_string();
+
         let data = format!("{{\"username\": \"{}\", \"password\": \"{}\"}}", username.clone(), password.clone());
         let uri = format!("{}/login", controller.clone());
 
         let response = client
             .post(uri)
-            .headers(AviClient::headers("missing").await?)
+            .headers(AviClient::default_headers(&version, &tenant).await?)
             .body(data)
             .send()
             .await?;
@@ -58,7 +64,7 @@ impl AviClient {
             None => ("error".to_string(), 0u64)
         };
 
-        let (session,_session_max_age) = match response.cookies().find(|x| x.name() == "avi-sessionid").map(|x| (x.value().to_string(), x.max_age().unwrap().as_secs())) {
+        let (session,session_max_age) = match response.cookies().find(|x| x.name() == "avi-sessionid").map(|x| (x.value().to_string(), x.max_age().unwrap().as_secs())) {
             Some(e) => {
                 log::debug!("Got back sessionid of: {}", e.0);
                 (e.0, e.1)
@@ -66,8 +72,9 @@ impl AviClient {
             None => ("error".to_string(), 0u64)
         };
 
-        let expires = Utc::now().timestamp() + token_max_age as i64;
-        Ok(Self { client, expires, username, password, controller, session, token })
+        let token_expires = Utc::now().timestamp() + token_max_age as i64;
+        let session_expires = Utc::now().timestamp() + session_max_age as i64;
+        Ok(Self { client, token_expires, session_expires, username, password, controller, session, token, tenant, version })
 
     }
 
@@ -76,7 +83,7 @@ impl AviClient {
         let uri = format!("{}/{}", path, self.controller);
         let response = self.client
             .delete(uri)
-            .headers(AviClient::headers(&self.token).await?)
+            .headers(self.headers().await?)
             .send()
             .await?;
 
@@ -90,7 +97,7 @@ impl AviClient {
         let uri = format!("{}/{}", path, self.controller);
         let response = self.client
             .post(uri)
-            .headers(AviClient::headers(&self.token).await?)
+            .headers(self.headers().await?)
             .body(body.to_string())
             .send()
             .await?;
@@ -105,7 +112,7 @@ impl AviClient {
         let uri = format!("{}/{}", path, self.controller);
         let response = self.client
             .put(uri)
-            .headers(AviClient::headers(&self.token).await?)
+            .headers(self.headers().await?)
             .body(body.to_string())
             .send()
             .await?;
@@ -120,7 +127,7 @@ impl AviClient {
         let uri = format!("{}/{}", path, self.controller);
         let response = self.client
             .patch(uri)
-            .headers(AviClient::headers(&self.token).await?)
+            .headers(self.headers().await?)
             .body(body.to_string())
             .send()
             .await?;
@@ -135,7 +142,7 @@ impl AviClient {
         let uri = format!("{}/{}", path, self.controller);
         let response = self.client
             .get(uri)
-            .headers(AviClient::headers(&self.token).await?)
+            .headers(self.headers().await?)
             .send()
             .await?;
 
@@ -156,7 +163,7 @@ impl AviClient {
         log::info!("Getting json output from {}", &uri);
         let response = self.client
             .get(uri)
-            .headers(AviClient::headers(&self.token).await?)
+            .headers(self.headers().await?)
             .send()
             .await?;
 
@@ -191,52 +198,85 @@ impl AviClient {
 
         let response = self.client
             .post(uri)
-            .headers(AviClient::headers(&self.token).await?)
+            .headers(AviClient::default_headers(&self.version, &self.tenant).await?)
             .body(data)
             .send()
             .await?;
 
         match response.status() {
             reqwest::StatusCode::OK => log::info!("Successfully logged in to {}", self.controller.clone()),
-            _ => log::error!("Error logging in to controller: {}", response.status())
-        };
-
-        // Will need to handle bad logins somehow
-        let expires = match response.cookies().find(|x| x.name() == "avi-sessionid").map(|x| (x.value().to_string(), x.max_age().unwrap().as_secs())) {
-            Some(e) => {
-                match e.0 == self.token {
-                    true => log::info!("csrf token is the same as before..."),
-                    false => { 
-                        log::info!("Picked up new csrf token");
-                        log::debug!("Registered crsf token: {}", e.0);
-                        self.token = e.0;
-                    }
-                };
-
-                // Update max_age for new token
-                let new_expires = Utc::now().timestamp() + e.1 as i64;
-                self.expires = new_expires;
-                self.expires
-            },
-            None => {
-                log::info!("Failed getting csrf token");
-                self.expires = 0;
-                self.expires
+            _ => {
+                log::error!("Error logging in to controller: {}", response.status());
+                return Ok("Error logging in to controller".to_owned());
             }
         };
-        Ok(format!("{}", expires))
+
+        let (token,token_max_age) = match response.cookies().find(|x| x.name() == "csrftoken").map(|x| (x.value().to_string(), x.max_age().unwrap().as_secs())) {
+            Some(e) => {
+                log::debug!("Got back csrf token of: {}", e.0);
+                (e.0, e.1)
+            },
+            None => ("error".to_string(), 0u64)
+        };
+
+        let (session,session_max_age) = match response.cookies().find(|x| x.name() == "avi-sessionid").map(|x| (x.value().to_string(), x.max_age().unwrap().as_secs())) {
+            Some(e) => {
+                log::debug!("Got back sessionid of: {}", e.0);
+                (e.0, e.1)
+            },
+            None => ("error".to_string(), 0u64)
+        };
+
+        match token == self.token {
+            true => log::info!("csrf token is the same as before..."),
+            false => { 
+                log::info!("Picked up new csrf token");
+                log::debug!("Registered crsf token: {}", &token);
+                self.token = token;
+            }
+        };
+
+        // Update max_age for new token
+        let new_token_expires = Utc::now().timestamp() + token_max_age as i64;
+        let new_session_expires = Utc::now().timestamp() + session_max_age as i64;
+        self.token_expires = new_token_expires;
+        self.session_expires = new_session_expires;
+        self.session = session;
+
+        Ok(self.token_expires.to_string())
     }
 
-    pub async fn headers(token: &str) -> BoxResult<HeaderMap> {
+    pub async fn default_headers(version: &str, tenant: &str) -> BoxResult<HeaderMap> {
         // Create HeaderMap
         let mut headers = HeaderMap::new();
 
-        log::debug!("Using X-CSRFToken of {}", &token);
+        // Add all headers
+        headers.insert("X-Avi-Version", HeaderValue::from_str(version).unwrap());
+        headers.insert("X-Avi-Tenant", HeaderValue::from_str(tenant).unwrap());
+        headers.insert(USER_AGENT, HeaderValue::from_str("hyper-rs").unwrap());
+        headers.insert(
+            CONTENT_TYPE,
+            HeaderValue::from_str("application/json").unwrap(),
+        );
+        headers.insert(
+            ACCEPT_ENCODING,
+            HeaderValue::from_str("application/json").unwrap(),
+        );
+
+        // Return headers
+        Ok(headers)
+    }
+
+    pub async fn headers(&self) -> BoxResult<HeaderMap> {
+        // Create HeaderMap
+        let mut headers = HeaderMap::new();
+
+        log::debug!("Using X-CSRFToken of {}", &self.token);
 
         // Add all headers
-        headers.insert("X-Avi-Version", HeaderValue::from_str("21.1.3").unwrap());
-        headers.insert("X-Avi-Tenant", HeaderValue::from_str("ebdc_kub3_dds_np").unwrap());
-        headers.insert("X-CSRFToken", HeaderValue::from_str(token).unwrap());
+        headers.insert("X-Avi-Version", HeaderValue::from_str(&self.version).unwrap());
+        headers.insert("X-Avi-Tenant", HeaderValue::from_str(&self.tenant).unwrap());
+        headers.insert("X-CSRFToken", HeaderValue::from_str(&self.token).unwrap());
         headers.insert(USER_AGENT, HeaderValue::from_str("hyper-rs").unwrap());
         headers.insert(
             CONTENT_TYPE,
@@ -253,14 +293,17 @@ impl AviClient {
 
     // Return back the time in UTC that the cookie will expire
     pub fn expires(&self) -> String {
-        let naive = NaiveDateTime::from_timestamp(self.expires, 0);
+        let naive = NaiveDateTime::from_timestamp(self.token_expires, 0);
         let datetime: DateTime<Utc> = DateTime::from_utc(naive, Utc);
         let newdate = datetime.format("%Y-%m-%d %H:%M:%S");
         newdate.to_string()
     }
 
     async fn renew(&mut self) -> BoxResult<()> {
-        if self.expires - Utc::now().timestamp() <= 0 {
+        if self.token_expires - Utc::now().timestamp() <= 0 {
+            log::info!("renew function kicking off re-login function");
+            self.login().await?;
+        } else if self.session_expires - Utc::now().timestamp() <= 0 {
             log::info!("renew function kicking off re-login function");
             self.login().await?;
         }
